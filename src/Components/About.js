b0890d866c8067fcf59b094f2e8a1ec2a65adfec
MustafaAltun2 <51758662+MustafaAltun2@users.noreply.github.com>
2020-01-31 19:08:21 +0100

    second commit

---
+++ src/Components/About.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../App.css';
+
+function About(){
+  
+    return (
+      <div className="Wrapper">
+        <section className="About">
+          
+            <div className="About-Conmtent">
+                <h3>Hi, I am Mustafa</h3>
+                <p>
+                    I am a programming student, I am always eager to learn a new language
+                     or improve code visibility for people new in the project group/team. 
+                     Over the years I have learned quite a bit about programming.
+                     I have a solid 2+ years of extensive experience in HTML5, CSS3, javascript, bootstrap 3,
+                    SASS/SCSS, react and wordpress also basic python. I am specialized in CSS3 with a keen attention
+                    to design. Currently learning C# and python(GDscript).<br/><br/>
+                    I believe every hard obstacle can be overcome through sheer commitment and hardwork. Always up for learning new things. 
+                </p>
+                <div className="Bonus">
+                  <h3><b>About this Portfolio</b></h3>
+                  <p>This website is made by Mustafa Altun with:
+                  <li>HTML5/CSS3</li>
+                  <li>Javascript</li>
+                  <li>A little bit of Jquery</li>
+                  <li>And everything on this site is made with react.</li>
+                  <div id="Project1"></div>
+                  </p>
+                </div>
+            </div>
+        </section>
+      </div>
+    );
+  } 
+
+
+
+export default About;
