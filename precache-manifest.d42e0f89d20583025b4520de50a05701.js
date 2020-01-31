c42de686a6504a266987fc5f94b0024c4e4e31a4
MustafaAltun2 <51758662+MustafaAltun2@users.noreply.github.com>
2020-01-31 12:13:40 +0100

    Updates

---
+++ precache-manifest.d42e0f89d20583025b4520de50a05701.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9296a44bec4b803d5f4ecbad1b86dc11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "954284c30566b8fc79a6",
+    "url": "/static/css/main.4036742a.chunk.css"
+  },
+  {
+    "revision": "10db054e2fb2be8112fe",
+    "url": "/static/js/2.098807f8.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/static/js/2.098807f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "954284c30566b8fc79a6",
+    "url": "/static/js/main.90fa18be.chunk.js"
+  },
+  {
+    "revision": "4a13974e532312d2dfc3",
+    "url": "/static/js/runtime-main.3d355337.js"
+  },
+  {
+    "revision": "b6bae0c91340b58a9710c78351dc01d2",
+    "url": "/static/media/BGimg2.b6bae0c9.jpg"
+  },
+  {
+    "revision": "f6938dcc0b4337b42cb2cb9ba831f59e",
+    "url": "/static/media/BGimg3.f6938dcc.jpg"
+  },
+  {
+    "revision": "a25822cb35e8e8d257a7e185d976457b",
+    "url": "/static/media/BGimg4.a25822cb.jpg"
+  },
+  {
+    "revision": "5f170311756e43c02033f09c292ea23a",
+    "url": "/static/media/Game1.5f170311.PNG"
+  },
+  {
+    "revision": "f9db45e6132afececb2036aaba042e6d",
+    "url": "/static/media/Game2.f9db45e6.PNG"
+  },
+  {
+    "revision": "1b9bc4307fe2e24a69f6dcbe545eedc3",
+    "url": "/static/media/Game3.1b9bc430.PNG"
+  },
+  {
+    "revision": "4006cc0a751d5915b6a66747fa29ed3c",
+    "url": "/static/media/img1.4006cc0a.jpg"
+  },
+  {
+    "revision": "c357a31526ad8a93ed1bfc3570305977",
+    "url": "/static/media/project1-school.c357a315.PNG"
+  },
+  {
+    "revision": "7aa798207fbefd1ae929ec9ad2d495ec",
+    "url": "/static/media/project2-school.7aa79820.PNG"
+  }
+]);
