044c2d6817399d11743e9276ec0cb89d9eee7644
MustafaAltun2 <51758662+MustafaAltun2@users.noreply.github.com>
2020-02-16 21:23:20 +0100

    third commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -14,7 +14,7 @@ function Contact(){
               <div className="Footer-Content">
                 <p className="Credit">&copy;Copyright 2020 Mustafa Altun. <br/>Developed by Mustafa Altun.</p>
                 <ul className="Social">
-                    <li><a href="https://www.linkedin.com/in/mustafa-altun-4196a11a0/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                    <li><a href="https://github.com/MustacheAltun/MustacheAltun.github.io" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
                     <li><a href="https://github.com/MustafaAltun2?tab=repositories" target="_blank" rel="noopener noreferrer">Github</a></li>
                     <li><a href="mailto:9004231@student.zadkine.nl" target="_blank" rel="noopener noreferrer">Mail</a></li>
 
